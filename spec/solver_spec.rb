198d0645e231ac9f9ce18dee5b28c68fc0f0bb91
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 20:44:47 +0300

    Create solver.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,7 +11,15 @@ describe Solver do
     actual = subject.factorial(integer)
         # Assert 
     expect(actual).to eql factorial
-
+    end
+    it 'factorial of 0 is 1' do
+            # Arrange
+        integer = 0
+        factorial = 1
+            # Act 
+        actual = subject.factorial(integer)
+            # Assert 
+        expect(actual).to eql factorial
     end
   end
 end
