552502db283651a1f99641357e388f0196fdc533
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 22:21:09 +0300

    Update solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,32 +1,35 @@
 require_relative 'spec_helper'
 
 describe Solver do
-    subject { Solver.new }
+  subject { Solver.new }
   context '#factorial' do
     it 'takes integer and returns factorial' do
-        # Arrange
-    integer = 5
-    factorial = 120
-        # Act 
-    actual = subject.factorial(integer)
-        # Assert 
-    expect(actual).to eql factorial
+      # Arrange
+      integer = 5
+      factorial = 120
+      # Act
+      actual = subject.factorial(integer)
+      # Assert
+      expect(actual).to eql factorial
     end
     it 'factorial of 0 is 1' do
-            # Arrange
-        integer = 0
-        factorial = 1
-            # Act 
-        actual = subject.factorial(integer)
-            # Assert 
-        expect(actual).to eql factorial
+      # Arrange
+      integer = 0
+      factorial = 1
+      # Act
+      actual = subject.factorial(integer)
+      # Assert
+      expect(actual).to eql factorial
     end
     it 'raises exception for negative integer' do
-            # Arrange
-        integer = -1
-        
-            # Act & Assert
-        expect { subject.factorial(integer) }.to raise_error('exception error')
+      # Arrange
+      integer = -1
+
+      # Act & Assert
+      expect { subject.factorial(integer) }.to raise_error('exception error')
+    end
+  end
+
   context '#reverse' do
     it 'takes a string and return it reversed' do
       # Arrange
