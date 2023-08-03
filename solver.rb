dfd36c5c427f9b978fbe6b6c4edf353cceae3934
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 22:21:16 +0300

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -1,11 +1,16 @@
 class Solver
-    def factorial(integer)
-        raise 'error' if integer < 0
-        factorial = 1
-        integer.times do 
-            factorial *= integer    
-            integer-=1
-        end
-        factorial
+  def factorial(integer)
+    raise 'exception error' if integer.negative?
+
+    factorial = 1
+    integer.times do
+      factorial *= integer
+      integer -= 1
     end
+    factorial
+  end
+
+  def reverse(string)
+    string.reverse
+  end
 end
