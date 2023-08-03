3005f91a66a5606cb5c9f568cbde2919f58fb3fc
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-03 20:39:19 +0300

    Update solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,17 @@
 require_relative 'spec_helper'
 
 describe Solver do
+    subject { Solver.new }
   context '#factorial' do
+    it 'takes integer and returns factorial' do
+        # Arrange
+    integer = 5
+    factorial = 120
+        # Act 
+    actual = subject.factorial(integer)
+        # Assert 
+    expect(actual).to eql factorial
+
+    end
   end
 end
